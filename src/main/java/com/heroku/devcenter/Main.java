5b81a59fbe115a00a00fec7357a8cb5885865874
Ryan Brainard <brainard@heroku.com>
2012-02-06 17:55:26 -0800

    shutdown memcache client in sample

--- src/main/java/com/heroku/devcenter/Main.java
+++ src/main/java/com/heroku/devcenter/Main.java
@@ -1,16 +1,16 @@
 package com.heroku.devcenter;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.URISyntaxException;
-import java.util.Collections;
-
 import net.spy.memcached.ConnectionFactory;
 import net.spy.memcached.ConnectionFactoryBuilder;
 import net.spy.memcached.ConnectionFactoryBuilder.Protocol;
 import net.spy.memcached.MemcachedClient;
 import net.spy.memcached.auth.AuthDescriptor;
 import net.spy.memcached.auth.PlainCallbackHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URISyntaxException;
+import java.util.Collections;
 
 public class Main {
 
@@ -24,10 +24,11 @@ public class Main {
                 new PlainCallbackHandler(System.getenv("MEMCACHE_USERNAME"), System.getenv("MEMCACHE_PASSWORD")));
 		ConnectionFactoryBuilder factoryBuilder = new ConnectionFactoryBuilder();
 		ConnectionFactory cf = factoryBuilder.setProtocol(Protocol.BINARY).setAuthDescriptor(ad).build();
-		
+
 		MemcachedClient memcachedClient = new MemcachedClient(cf, Collections.singletonList(new InetSocketAddress(System.getenv("MEMCACHE_SERVERS"), 11211)));
 		memcachedClient.add("test", 0, "testData");
 		System.out.println(memcachedClient.get("test"));
+        memcachedClient.shutdown();
 	}
 
 }
--- src/main/java/com/heroku/devcenter/spring/Main.java
+++ src/main/java/com/heroku/devcenter/spring/Main.java
@@ -1,20 +1,11 @@
 package com.heroku.devcenter.spring;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.URISyntaxException;
-import java.util.Collections;
-
-import net.spy.memcached.ConnectionFactory;
-import net.spy.memcached.ConnectionFactoryBuilder;
-import net.spy.memcached.ConnectionFactoryBuilder.Protocol;
 import net.spy.memcached.MemcachedClient;
-import net.spy.memcached.auth.AuthDescriptor;
-import net.spy.memcached.auth.PlainCallbackHandler;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main {
 
@@ -30,6 +21,7 @@ public class Main {
 		
 		memcachedClient.add("testSpring", 0, "testDataSpring");
 		System.out.println(memcachedClient.get("testSpring"));
+        memcachedClient.shutdown();
 	}
 
 }
