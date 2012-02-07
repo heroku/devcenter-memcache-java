d37c3994f23dcbad719170ff0def10f5c1cbbc2a
Ryan Brainard <brainard@heroku.com>
2012-02-06 18:04:59 -0800

    replace tabs with spaces in Main classes

--- src/main/java/com/heroku/devcenter/Main.java
+++ src/main/java/com/heroku/devcenter/Main.java
@@ -14,21 +14,21 @@ import java.util.Collections;
 
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws URISyntaxException 
-	 */
-	public static void main(String[] args) throws IOException, URISyntaxException {
-		AuthDescriptor ad = new AuthDescriptor(new String[]{"PLAIN"},
+    /**
+     * @param args
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        AuthDescriptor ad = new AuthDescriptor(new String[]{"PLAIN"},
                 new PlainCallbackHandler(System.getenv("MEMCACHE_USERNAME"), System.getenv("MEMCACHE_PASSWORD")));
-		ConnectionFactoryBuilder factoryBuilder = new ConnectionFactoryBuilder();
-		ConnectionFactory cf = factoryBuilder.setProtocol(Protocol.BINARY).setAuthDescriptor(ad).build();
+        ConnectionFactoryBuilder factoryBuilder = new ConnectionFactoryBuilder();
+        ConnectionFactory cf = factoryBuilder.setProtocol(Protocol.BINARY).setAuthDescriptor(ad).build();
 
-		MemcachedClient memcachedClient = new MemcachedClient(cf, Collections.singletonList(new InetSocketAddress(System.getenv("MEMCACHE_SERVERS"), 11211)));
-		memcachedClient.add("test", 0, "testData");
-		System.out.println(memcachedClient.get("test"));
+        MemcachedClient memcachedClient = new MemcachedClient(cf, Collections.singletonList(new InetSocketAddress(System.getenv("MEMCACHE_SERVERS"), 11211)));
+        memcachedClient.add("test", 0, "testData");
+        System.out.println(memcachedClient.get("test"));
         memcachedClient.shutdown();
-	}
+    }
 
 }
--- src/main/java/com/heroku/devcenter/spring/Main.java
+++ src/main/java/com/heroku/devcenter/spring/Main.java
@@ -9,19 +9,19 @@ import java.net.URISyntaxException;
 
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws URISyntaxException 
-	 */
-	public static void main(String[] args) throws IOException, URISyntaxException {
-		//ApplicationContext ctx = new GenericXmlApplicationContext("applicationContext.xml");
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
-		MemcachedClient memcachedClient = ctx.getBean(MemcachedClient.class);
-		
-		memcachedClient.add("testSpring", 0, "testDataSpring");
-		System.out.println(memcachedClient.get("testSpring"));
+    /**
+     * @param args
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        //ApplicationContext ctx = new GenericXmlApplicationContext("applicationContext.xml");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        MemcachedClient memcachedClient = ctx.getBean(MemcachedClient.class);
+
+        memcachedClient.add("testSpring", 0, "testDataSpring");
+        System.out.println(memcachedClient.get("testSpring"));
         memcachedClient.shutdown();
-	}
+    }
 
 }
